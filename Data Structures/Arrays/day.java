7f04a353c8342dcf28d6d70a5f7b2c2368f3b61e
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-07-30 22:54:17 +0530

    Update and rename Solution.java to day.java

--- Data Structures/Arrays/day.java
+++ Data Structures/Arrays/day.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class day {
     public static void main(String[] args) {
         String[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
         for (String day : days) {
