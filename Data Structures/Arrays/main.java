ff0771fed0e4d0a9b81a202ddf328d2dd10c9711
Aniket Sundriyal <sundriyalaniket@gmail.com>
2025-07-30 23:07:18 +0530

    Create main.java

---
+++ Data Structures/Arrays/main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] monthsOfYear = {
+            "January", "February", "March", "April",
+            "May", "June", "July", "August",
+            "September", "October", "November", "December"
+        };
+
+        // Optional: print all months
+        for (String month : monthsOfYear) {
+            System.out.println(month);
+        }
+    }
+}
